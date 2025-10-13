9683017146d54a0af8c292a87c31c5377131234c
Phạm Minh Thông <thongphamminh3@gmail.com>
2025-10-13 21:41:17 +0700

    Fix: fix error select on Cemetery

---
+++ app/Filament/Resources/CemeteryResource/Pages/EditCemetery.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Filament\Resources\CemeteryResource\Pages;
+
+use App\Filament\Resources\CemeteryResource;
+use Filament\Actions;
+use Filament\Resources\Pages\EditRecord;
+
+class EditCemetery extends EditRecord
+{
+    protected static string $resource = CemeteryResource::class;
+
+    protected function getHeaderActions(): array
+    {
+        return [
+            Actions\ViewAction::make(),
+            Actions\DeleteAction::make(),
+        ];
+    }
+
+    protected function getRedirectUrl(): string
+    {
+        return $this->getResource()::getUrl('index');
+    }
+}
