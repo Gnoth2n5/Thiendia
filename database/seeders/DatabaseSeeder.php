883fd38e05dca76f7d9438d16bbd6c8179f45b61
Phạm Minh Thông <thongphamminh3@gmail.com>
2025-11-03 23:12:18 +0700

    Refactor seeders to streamline cemetery and user creation processes

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,8 +2,6 @@
 
 namespace Database\Seeders;
 
-use App\Models\User;
-// use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -13,23 +11,11 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // Tạo admin user trước
-        if (! User::where('email', 'admin@gmail.com')->exists()) {
-            User::factory()->create([
-                'name' => 'Admin User',
-                'email' => 'admin@gmail.com',
-                'password' => bcrypt('password'),
-                'role' => 'admin',
-            ]);
-        }
-
         // Chạy các seeder theo thứ tự
         $this->call([
             UserSeeder::class,
             CemeterySeeder::class,
-            GraveSeeder::class,
-            DeceasedPersonSeeder::class,
-            ArticleSeeder::class,
+            CemeteryPlotSeeder::class,
         ]);
     }
 }
--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -2,7 +2,6 @@
 
 namespace Database\Seeders;
 
-use App\Http\Controllers\HomeController;
 use App\Models\User;
 use Illuminate\Database\Seeder;
 
@@ -13,185 +12,26 @@ class UserSeeder extends Seeder
      */
     public function run(): void
     {
-        // Tạo admin user nếu chưa tồn tại
-        if (! User::where('email', 'admin@gmail.com')->exists()) {
-            User::create([
-                'name' => 'Admin User',
-                'email' => 'admin@gmail.com',
-                'password' => bcrypt('password'),
-                'role' => 'admin',
-                'commune' => null,
-            ]);
-        }
+        // Tạo admin user
+        User::create([
+            'name' => 'Admin',
+            'email' => 'admin@gmail.com',
+            'password' => bcrypt('password'),
+            'role' => 'admin',
+            'commune' => null,
+        ]);
 
-        // Lấy danh sách xã/phường từ API
-        $controller = new HomeController;
-        $wards = $controller->fetchAndCacheWards();
+        $this->command->info('Created admin user: admin@gmail.com');
 
-        // Tạo tài khoản cho 10 xã/phường đầu tiên (ví dụ)
-        $sampleWards = array_slice($wards, 0, 10);
+        // Tạo cán bộ xã Lý Nhân
+        User::create([
+            'name' => 'Cán bộ Xã Lý Nhân',
+            'email' => 'canbo.lynhan@gmail.com',
+            'password' => bcrypt('password'),
+            'role' => 'commune_staff',
+            'commune' => 'Lý Nhân',
+        ]);
 
-        foreach ($sampleWards as $ward) {
-            $wardName = $ward['name'];
-            $email = 'canbo.'.strtolower(str_replace(' ', '', $this->removeVietnameseTones($wardName))).'@gmail.com';
-
-            // Kiểm tra nếu chưa tồn tại
-            if (! User::where('email', $email)->exists()) {
-                User::create([
-                    'name' => "Cán bộ {$ward['type']} {$wardName}",
-                    'email' => $email,
-                    'password' => bcrypt('password'),
-                    'role' => 'commune_staff',
-                    'commune' => $wardName,
-                ]);
-            }
-        }
-    }
-
-    /**
-     * Remove Vietnamese tones from string.
-     */
-    private function removeVietnameseTones(string $str): string
-    {
-        $vietnameseTones = [
-            'à',
-            'á',
-            'ạ',
-            'ả',
-            'ã',
-            'â',
-            'ầ',
-            'ấ',
-            'ậ',
-            'ẩ',
-            'ẫ',
-            'ă',
-            'ằ',
-            'ắ',
-            'ặ',
-            'ẳ',
-            'ẵ',
-            'è',
-            'é',
-            'ẹ',
-            'ẻ',
-            'ẽ',
-            'ê',
-            'ề',
-            'ế',
-            'ệ',
-            'ể',
-            'ễ',
-            'ì',
-            'í',
-            'ị',
-            'ỉ',
-            'ĩ',
-            'ò',
-            'ó',
-            'ọ',
-            'ỏ',
-            'õ',
-            'ô',
-            'ồ',
-            'ố',
-            'ộ',
-            'ổ',
-            'ỗ',
-            'ơ',
-            'ờ',
-            'ớ',
-            'ợ',
-            'ở',
-            'ỡ',
-            'ù',
-            'ú',
-            'ụ',
-            'ủ',
-            'ũ',
-            'ư',
-            'ừ',
-            'ứ',
-            'ự',
-            'ử',
-            'ữ',
-            'ỳ',
-            'ý',
-            'ỵ',
-            'ỷ',
-            'ỹ',
-            'đ',
-        ];
-        $replacement = [
-            'a',
-            'a',
-            'a',
-            'a',
-            'a',
-            'a',
-            'a',
-            'a',
-            'a',
-            'a',
-            'a',
-            'a',
-            'a',
-            'a',
-            'a',
-            'a',
-            'a',
-            'e',
-            'e',
-            'e',
-            'e',
-            'e',
-            'e',
-            'e',
-            'e',
-            'e',
-            'e',
-            'e',
-            'i',
-            'i',
-            'i',
-            'i',
-            'i',
-            'o',
-            'o',
-            'o',
-            'o',
-            'o',
-            'o',
-            'o',
-            'o',
-            'o',
-            'o',
-            'o',
-            'o',
-            'o',
-            'o',
-            'o',
-            'o',
-            'o',
-            'u',
-            'u',
-            'u',
-            'u',
-            'u',
-            'u',
-            'u',
-            'u',
-            'u',
-            'u',
-            'u',
-            'y',
-            'y',
-            'y',
-            'y',
-            'y',
-            'd',
-        ];
-
-        return str_replace($vietnameseTones, $replacement, $str);
+        $this->command->info('Created commune staff user: canbo.lynhan@gmail.com');
     }
 }
